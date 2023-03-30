eb3b4487fc58cc1920ec00bc88354568495a692b
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-31 01:33:33 +0300

    Finish task number one 	modified:   src/js/01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// Отслеживаю список с классом gallery
+const galleryElement = document.querySelector('.gallery')
+
+// Создаю разметку по массиву galleryItems
+function getGalleryItems(galleryItems) { 
+ return  galleryItems.map(function (element) {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${element.original}">
+      <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+   </a>
+</li>`
+   }).join('')
+}
+// Добавляю разметку в список gallery
+galleryElement.innerHTML = getGalleryItems(galleryItems)
+
+// Использую библиотеку SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', { captionSelector: 'img', captionType: 'attr', captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom' });
+
