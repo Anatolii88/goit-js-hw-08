286294863fd4718b3190443614eb880b10bc4243
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-04-03 00:31:23 +0300

    Finish three task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,6 @@
+// Импортирую библиотеку
 import throttle from 'lodash.throttle';
+//Импортирую библиотеку
 import Player from '@vimeo/player';
 
 
@@ -9,6 +11,8 @@ import Player from '@vimeo/player';
         console.log('played the video!');
     });
 
+    /* Применяю событие  "timeupdate" , библиотеку  lodash 
+    и сохраняю время воспроизведения в локальное хранилище */
      player.on('timeupdate', throttle(function (event) {
       const seconds = event.seconds
       localStorage.setItem('videoplayer-current-time',seconds)
@@ -18,6 +22,14 @@ import Player from '@vimeo/player';
         console.log('title:', title);
     });
 
-       const playerSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'))
-  
-      player.setCurrentTime(playerSeconds)
+// Получаю значения  из локального хранилища
+const playerSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+
+  /* Применяю метод setCurrentTime при условии ,
+   если значение в локальном хранилище было больше 0 */
+function onSeconds() { 
+  if (playerSeconds) { 
+    player.setCurrentTime(playerSeconds)
+  }
+}
+onSeconds();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+// Импортирую библиотеку
+import throttle from 'lodash.throttle';  
+
+// Нахожу форму
+const form = document.querySelector('.feedback-form') 
+
+// Создаю объект , в котором будут храниться значения импута 
+const formData = {}; 
+getValueLocalStorage() 
+
+// Добавляю слушателя на форму
+form.addEventListener('submit', onSubmit)
+
+
+// Очищаю поля инпутов и удаляю ключ на локальном хранилище
+function onSubmit(evt) {
+  evt.preventDefault()
+  console.log(formData)
+  evt.currentTarget.reset()
+    localStorage.removeItem('feedback-form-state')
+ }
+
+
+// Ставлю слушателя на форму , использую бибилиотеку lodash
+form.addEventListener('input', throttle(onInput, 500))
+
+/*  Записываю значения инпутов в объект и
+  сохраняю его в виде строки  на локальном хранилище */
+function onInput(evt) {
+  evt.preventDefault()
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+}
+ 
+// Возвращаю  свойства объекта  с локального хранилища  в значения инпутов 
+function getValueLocalStorage() { 
+  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'))
+  if (savedMessage) { 
+    Object.entries(savedMessage).forEach(function ([name,value]) { 
+      form.elements[name].value = value;
+    })
+  }
+};
+
 
