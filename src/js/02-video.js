3381e6b828a8b5446df3dc1c1aaa5186ed86fcb9
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-04-02 18:02:53 +0300

    Finish two task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+   const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+     player.on('timeupdate', throttle(function (event) {
+      const seconds = event.seconds
+      localStorage.setItem('videoplayer-current-time',seconds)
+    }, 1000) );
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+       const playerSeconds = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  
+      player.setCurrentTime(playerSeconds)
